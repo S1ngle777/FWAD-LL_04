89dec58d57d1086a73e537456eb50a090eae46d2
S1ngle777 <nniebogha@gmail.com>
2024-11-24 04:48:16 +0200

    first commit

---
+++ todo-app/resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'Default Title')</title>
+    @vite(['resources/css/app.css'])
+</head>
+<body>
+    <x-header>
+        @yield('header', 'Default Header')
+    </x-header>
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
